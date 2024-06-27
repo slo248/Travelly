be3e0f6e592ee9ed375ac176d807fee2c370a3eb
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-06-27 20:39:36 +0700

    apply fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { rMS } from './styles/responsive';
+import { Fonts } from './styles/fonts';
 
 export default function App() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ fontSize: rMS(20) }}>App</Text>
+      <Text style={{ fontSize: rMS(20), fontFamily: Fonts.regular }}>
+        Hello World!
+      </Text>
     </View>
   );
 }
---
+++ src/styles/fonts.ts
@@ -0,0 +1,12 @@
+export const Fonts = {
+  extraBold: 'Poppins-ExtraBold',
+  bold: 'Poppins-Bold',
+  semiBold: 'Poppins-SemiBold',
+  regular: 'Poppins-Regular',
+  medium: 'Poppins-Medium',
+  light: 'Poppins-Light',
+  extraLight: 'Poppins-ExtraLight',
+  thin: 'Poppins-Thin'
+};
+
+export type FontType = keyof typeof Fonts;
