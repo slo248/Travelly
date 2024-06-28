f0c7f34a2fc34c86dc38f40781c5b0e880308d51
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-06-28 21:25:52 +0700

    add images

---
+++ src/data/onboarding.ts
@@ -0,0 +1,14 @@
+export const onBoardingData = [
+  {
+    description: 'Booking tickets for planes and trains',
+    image: require('~/assets/imgs/onboarding/1.png')
+  },
+  {
+    description: 'Booking tickets for planes and trains',
+    image: require('~/assets/imgs/onboarding/2.png')
+  },
+  {
+    description: 'Booking tickets for planes and trains',
+    image: require('~/assets/imgs/onboarding/3.png')
+  }
+];
