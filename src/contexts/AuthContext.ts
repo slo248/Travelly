410dae7ae2206b6b8eeb423d6f1095dc6f8364a9
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-07-02 19:14:27 +0700

    add user to context

--- src/contexts/AuthContext.ts
+++ src/contexts/AuthContext.ts
@@ -1,6 +1,9 @@
 import { createContext } from 'react';
 
+import { user } from '~/data/user';
+
 export const AuthContext = createContext({
   signIn: () => {},
-  signOut: () => {}
+  signOut: () => {},
+  user
 });
