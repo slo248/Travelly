2c9e5b1d21203b575eb17ab46907bcf3e43e1d88
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-07-05 11:31:13 +0700

    add custom nav bar

--- src/navigators/BookingStack.tsx
+++ src/navigators/BookingStack.tsx
@@ -5,7 +5,7 @@ import {
   useNavigation
 } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { bookingSchema } from '~/constants/schemas/booking';
 import Flights from '~/screens/Booking/Flights';
@@ -13,6 +13,8 @@ import Filter from '~/screens/Booking/Filter';
 import HomeBooking from '~/screens/Booking/HomeBooking';
 import TransportBooking from '~/screens/Booking/TransportBooking';
 import { FlightsProvider } from '~/contexts/FlightsContext';
+import { View } from 'react-native';
+import { rH } from '~/styles/responsive';
 
 export type BookingStackParamList = {
   HomeBooking: undefined;
@@ -34,28 +36,48 @@ const BookingStack = () => {
         methods.reset();
         navigation.reset({
           index: 0,
-          routes: [{ name: 'HomeBooking' }]
+          routes: [{ name: 'TransportBooking' }]
         });
       };
     }, [])
   );
+
+  // useEffect(() => {
+  //   const hideTabBar = navigation.addListener('focus', () => {
+  //     navigation.setOptions({ tabBarStyle: { display: 'none' } });
+  //   });
+
+  //   const showTabBar = navigation.addListener('blur', () => {
+  //     navigation.setOptions({ tabBarStyle: { display: 'flex' } });
+  //   });
+
+  //   return () => {
+  //     hideTabBar();
+  //     showTabBar();
+  //   };
+  // }, [navigation]);
+
   return (
-    <FlightsProvider>
-      <FormProvider {...methods}>
-        <Stack.Navigator
-          initialRouteName="HomeBooking"
-          screenOptions={{
-            animation: 'slide_from_right',
-            headerShown: false
-          }}
-        >
-          <Stack.Screen name="HomeBooking" component={HomeBooking} />
-          <Stack.Screen name="TransportBooking" component={TransportBooking} />
-          <Stack.Screen name="Flights" component={Flights} />
-          <Stack.Screen name="Filter" component={Filter} />
-        </Stack.Navigator>
-      </FormProvider>
-    </FlightsProvider>
+    <View style={{ flex: 1, marginBottom: rH(12) }}>
+      <FlightsProvider>
+        <FormProvider {...methods}>
+          <Stack.Navigator
+            initialRouteName="TransportBooking"
+            screenOptions={{
+              animation: 'slide_from_right',
+              headerShown: false
+            }}
+          >
+            <Stack.Screen
+              name="TransportBooking"
+              component={TransportBooking}
+            />
+            <Stack.Screen name="Flights" component={Flights} />
+            <Stack.Screen name="Filter" component={Filter} />
+          </Stack.Navigator>
+        </FormProvider>
+      </FlightsProvider>
+    </View>
   );
 };
 
